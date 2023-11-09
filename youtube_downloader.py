6f5768ece67b03815cb3d2d5b883d4eb5ae41eb9
mncrftfrcnm <64576881+mncrftfrcnm@users.noreply.github.com>
2023-11-09 10:13:00 +0400

    Add files via upload

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -4,3 +4,4 @@ link = input('link: ')
 youtubeObject = YouTube(link)
 youtubeObject = youtubeObject.streams.get_highest_resolution()
 youtubeObject.download()
+# example link: https://www.youtube.com/watch?v=dQw4w9WgXcQ
